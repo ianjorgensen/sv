374f74ffaa086bd5e7d9c19d788f7efe7de07ecd
Ian Jorgensen <jorgensen.ian@gmail.com>
2012-04-09 13:29:51 +0200

    done

--- server.js
+++ server.js
@@ -3,17 +3,13 @@ var serve = require('./lib/serve');
 
 var port = process.argv[2] || 9090;
 
-server.get('/', serve.file('./html/splash.html'));
-
-server.get('/preview', serve.file('./html/home.html'));
-
 server.get('/js/*', serve.file('./js/{*}'));
 
 server.get('/css/*', serve.file('./css/{*}'));
 
 server.get('/img/*', serve.image('./img/{*}'));
 
-server.all('*', serve.file('./html/404.html'));
+server.all('*', serve.file('./html/sexy.html'));
 
 server.listen(port);
 
